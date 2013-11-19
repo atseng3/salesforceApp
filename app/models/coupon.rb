72478f78b62d083cbe7e6bd541de59a0bf4c93e8
Benjamin Lee <benjamsandwich@gmail.com>
2013-11-18 16:15:25 -0800

    Configure for AWS image storage, Figaro setup

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,5 +1,4 @@
 class Coupon < ActiveRecord::Base
   attr_accessible :user_id, :deal_id, :place, :address, :zip, :deal_title, :deal_info
-  validates :deal_id, :place, :address, :zip, :deal_title, :deal_info, presence: true
-  
+  validates :deal_id, :place, :address, :zip, :deal_title, :deal_info, presence: true  
 end
